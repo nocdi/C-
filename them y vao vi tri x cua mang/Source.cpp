f5f9366386207d386d1a6a59d486529b3b0e5092
nocdi <dangphamngocduy04@gmail.com>
2025-05-24 00:09:10 +0700

    Source.cpp

--- them y vao vi tri x cua mang/Source.cpp
+++ them y vao vi tri x cua mang/Source.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int mang[20];
+	int n, x, y;
+	cout << "Nhap phan tu n: ";
+	cin >> n;
+	cout << "Nhap mang co " << n << " phan tu: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Nhap phan tu thu a[" << i  << "]: ";
+		cin >> mang[i];
+	}
+	cout << "Nhap x: ";
+	cin >> x;
+	cout << "Nhap y: ";
+	cin >> y;
+	for (int i = n; i > x; i--) {
+		if (i - 1 >= 0 && i < 20) {
+			mang[i] = mang[i - 1];
+		}
+	}
 
+	mang[x] = y;
+	n++;
+
+	cout << "Mang sau khi them z la: ";
+	for (int i = 0; i < n; i++) {
+		cout << mang[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
