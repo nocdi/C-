eaf0040cedfe079e450193ae685b7f6bd637675f
nocdi <dangphamngocduy04@gmail.com>
2025-05-25 00:42:56 +0700

    Source.cpp

--- sapxepthututangdan/Source.cpp
+++ sapxepthututangdan/Source.cpp
@@ -13,7 +13,7 @@ int main() {
 	// Sap xep mang theo thu tu tang dan
     for (int i = 0; i < n - 1; i++) {
 		for (int j = 0; j < n - 1 - i; j++) {
-			if (arr[j] > arr[j + 1]) {
+			if (arr[j] > arr[j + 1]) { // muốn thay đổi từ tăng dần thành giảm dần thì thay dấu > thành <.
 				// Hoan doi hai phan tu
 				int temp = arr[j];
 				arr[j] = arr[j + 1];
@@ -27,3 +27,4 @@ int main() {
 	}
 	return 0;
 }
+// muốn giảm dần thì thay đổi giấu > thành < trong phần if.
