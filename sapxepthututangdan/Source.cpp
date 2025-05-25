e8ec32e3bccd57cb7b1014ff9c0e862d8cf25086
nocdi <dangphamngocduy04@gmail.com>
2025-05-26 00:23:32 +0700

    Update Source.cpp

--- sapxepthututangdan/Source.cpp
+++ sapxepthututangdan/Source.cpp
@@ -28,3 +28,37 @@ int main() {
 	return 0;
 }
 // muốn giảm dần thì thay đổi giấu > thành < trong phần if.
+
+
+
+
+#include <iostream>// cách 2
+
+using namespace std;
+
+void sortArray(int arr[1000], int n) {
+	for (int i = 0; i < n; i++) {
+		for(int j = i + 1; j < n; j++){
+			if (arr[i] < arr[j]) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	int n;
+	int arr[1000];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sortArray(arr, n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
